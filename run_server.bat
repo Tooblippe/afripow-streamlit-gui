c286ab43bc8fd0999e84f016d9af2831533511b4
tooblippe <tobie.nortje@gmail.com>
2024-06-12 11:48:24 +0200

    added mareginal cost y_lim

--- run_server.bat
+++ run_server.bat
@@ -1,4 +1,4 @@
-call C:\ProgramData\Anaconda3\condabin\activate.bat pypsa_v0_026_3_1
+call C:\Users\User\anaconda3\condabin\activate.bat pypsa_v28_py311
 python -m pip install -i https://test.pypi.org/simple/ afripow-pypsa --upgrade
 git pull
 streamlit run C:\Users\User\OneDrive\S(Pypsa)\afripow_gui\PypsaGui.py --server.runOnSave=True --theme.primaryColor="0098FF" --logger.level=error

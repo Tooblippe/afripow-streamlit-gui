849c53751616f5bd4cb95880f68982916ee307ed
tooblippe <tobie.nortje@gmail.com>
2024-06-12 19:02:16 +0200

    servere new setup

--- run_server.bat
+++ run_server.bat
@@ -1,6 +1,6 @@
-call C:\Users\User\anaconda3\condabin\activate.bat pypsa_v28_py311
+call C:\Users\apvse\anaconda3\condabin\activate.bat pypsa_v28_py311
 python -m pip install -i https://test.pypi.org/simple/ afripow-pypsa --upgrade
 git pull
-streamlit run C:\Users\User\OneDrive\S(Pypsa)-APV-server2\afripow_gui\PypsaGui.py --server.runOnSave=True --theme.primaryColor="0098FF" --logger.level=error
+streamlit run C:\Users\apvse\OneDrive\afripow-streamlit-gui\PypsaGui.py --server.runOnSave=True --theme.primaryColor="0098FF" --logger.level=error
 
 call conda deactivate

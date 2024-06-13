adb3a6a330061da91499cafe9cf6ca1d3e85b532
tooblippe <tobie.nortje@gmail.com>
2024-06-13 19:31:58 +0200

    servere new setup

--- run_server.bat
+++ run_server.bat
@@ -1,4 +1,4 @@
-call C:\Users\apvse\anaconda3\condabin\activate.bat pypsa_v28_py311
+call C:\Users\apvse\anaconda3\condabin\activate.bat pypsa_v26_py311
 python -m pip install -i https://test.pypi.org/simple/ afripow-pypsa --upgrade
 git pull
 streamlit run C:\Users\apvse\OneDrive\afripow-streamlit-gui\PypsaGui.py --server.runOnSave=True --theme.primaryColor="0098FF" --logger.level=error

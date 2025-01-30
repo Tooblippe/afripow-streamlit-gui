2e9b589d8bf2a97716a97f7cf631772fa48b31ee
tooblippe <tobie.nortje@gmail.com>
2025-01-30 16:19:49 +0200

    updated pycharm asynco setting

---
+++ run_server_pypsa_32.bat
@@ -0,0 +1,6 @@
+call C:\Users\apvse\anaconda3\condabin\activate.bat pypsa_v32_py311
+REM python -m pip install -i https://test.pypi.org/simple/ afripow-pypsa --upgrade
+git pull
+git checkout master
+streamlit run C:\Users\apvse\OneDrive\afripow-streamlit-gui\PypsaGui.py --server.runOnSave=True --theme.primaryColor="0098FF" --logger.level=error
+call conda deactivate

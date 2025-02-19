0ac003e0e23dc1220d77328912d45109abc41303
tooblippe <tobie.nortje@gmail.com>
2025-02-19 09:45:28 +0200

    Updated stats writeout, Gui changes

--- experiments/reserve/reserve_play.py
+++ experiments/reserve/reserve_play.py
@@ -19,9 +19,37 @@ m.add_variables(
     name="p_nom",
 )
 
+m.add_variables(
+    network.links.p_nom_opt,
+    network.links.p_nom_opt,
+    coords=[network.snapshots, network.links],
+    name="p_nom_opt",
+)
+
 p_nom = m.variables["p_nom"]
+p_nom_opt = m.variables["p_nom_opt"]
 
 reserve_sync = p_nom - p
+
+
 reserve_per_snapshot = reserve_sync.sum(dim="Link")
 
 m.add_constraints(reserve_per_snapshot >= 10000, name="Reserve_p_nom_min_p")
+
+
+(p_nom - p).where(xr.DataArray(p >= 1, coords=p.coords, dims=p.dims)).sum(dim="Link")
+(p_nom_opt - p).where(xr.DataArray(p >= 1, coords=p.coords, dims=p.dims)).sum(
+    dim="Link"
+)
+
+# mmmmmm
+(p - 1).sum(dim="Link") >= 0
+
+ mask = xr.DataArray((p - 1) >= 0, coords=p.coords, dims=p.dims, name="p_on_filter")
+
+p.where(mask, other=xr.DataArray(False, coords=p.coords, dims=p.dims))
+
+def c(p):
+    return p>=1
+
+p.where(c)

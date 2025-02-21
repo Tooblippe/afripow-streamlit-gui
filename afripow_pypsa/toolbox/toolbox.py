224daa526285d63e5c68f7677e8f981f87f83ab6
tooblippe <tobie.nortje@gmail.com>
2025-02-21 11:12:53 +0200

    added theme color

--- afripow_pypsa/toolbox/toolbox.py
+++ afripow_pypsa/toolbox/toolbox.py
@@ -20,6 +20,7 @@ from ..Report.Report import Report, __version__
 __version__ = __version__
 
 from ..helpers.direcory_cases import remove_non_directory_files, find_int_named_subdirs
+from ..pypsa_toolbox.reserves import add_all_reserve_constraints
 
 print(f"Using toolbox version: {__version__}")
 
@@ -541,206 +542,206 @@ def fix_link_battery_capacity(network: pypsa.Network, m: linopy.model):
     print("---------------------------")
 
 
-def add_reserve_constraint_per_level_and_area(
-    m: linopy.model,
-    network: pypsa.Network,
-    reserve_level: str,
-    area_link_list: dict,
-    reserve_capacity_needed: float,
-    constraint_name: str,
-):
-    """add a single reserve constraint, this is called by the constaints setup function and not used by user"""
-
-    # Dispatch variable
-    # All links are dispatched
-    Total_Dispatch = m.variables["Link-p"] * network.links[reserve_level]
-
-    Dispatch = Total_Dispatch.sel(Link=area_link_list["all_links"]).sum("Link")
-
-    # Scheduled extendable variable
-    # only extendable links is added
-    # Meet a reserve level in a area (extendable and non extandable ) total needs to meet the given reserve requirement
-    Total_Schedule_Extendable = (
-        m.variables["Link-p_nom"].sel(
-            {"Link-ext": area_link_list["extendable"]}
-        )  # Select all the extendable links
-        * network.links.loc[area_link_list["extendable"]][
-            reserve_level
-        ].to_list()  # Multiply each extendable link with its reserve level
-    )
-    Schedule_Extendable = (
-        Total_Schedule_Extendable.sum()
-    )  # Get the total extendable TODO - what type of var is this
-
-    # Scheduled non-extendable variable (scalar)
-    # scheduled non-extendable totel is the sum of all the p_nom * reserve_level
-    # contribution of each link to reserves
-    non_extantable_total_mw = sum(
-        area_link_list["non_extendable"]["p_nom"]
-        * network.links.loc[area_link_list["non_extendable"]["links"]][reserve_level]
-    )
-
-    # build constraint sentence
-    # check if there are links, but if the non extendable total is 0 don't bother
-    if (
-        len(area_link_list["non_extendable"]["links"]) > 0
-        and non_extantable_total_mw > 0
-    ):
-        # If extendable then build new capacity until you meet reserve requirement
-        print(
-            f"Total adjusted non extendable contrib to reserves {area_link_list['non_extendable']['links']}- {non_extantable_total_mw}"
-        )
-        Reserve_Constraint = (
-            Schedule_Extendable >= reserve_capacity_needed - non_extantable_total_mw
-        )
-    else:
-        Reserve_Constraint = Schedule_Extendable >= reserve_capacity_needed
-
-    # add the constraint to the linopy model
-    constraint = m.add_constraints(Reserve_Constraint, name=constraint_name)
-
-    print(f"{constraint}\n\n")
-
-
-def add_all_reserve_constraints(
-    network: pypsa.Network, m: linopy.model, reserves_file_path: str | Path
-):
-    """find all constraints from links.csv, and loads reserves.csv witht the RHS from Inputsfolder
-
-    To set up, in the links.csv file:
-    1. add a column called "reserve_area" and add the reserve areas to this column e.g - reserve_area_1, WesternCape, Gauteng. These names will be used in 'reserves.csv'.
-    2. add columns called "reserve_level_1", "reserve_level_2", "reserve_level_n" and add the weightings e.g. 0.2, 0.3. These names will be used in 'reserves.csv'.
-
-    links.csv
-    reserve_area        reserve_level_1         reserve_level_2     .....
-    ============        ===============         ===============     =====
-    reserve_area_1      0.9                     0.1
-    WesternCape         0                       0.7
-    Gauteng             0.4                     0.2
-
-
-    3. Create a 'reserves.csv' file and use the 'reserve_area' and 'reserve_level' names to build the file and column 'level_area'
-    the level_area column should be 'reserve_level_name_reserve_area'. The 'value_mw' is the RHS of the constraint
-
-    reserves.csv
-    level_area                          value_mw
-    ------------------------------      --------
-    reserve_level_1_reserve_area_1      100
-    reserve_level_1_WesternCape         30
-    reserve_level_1_Gauteng             20
-
-    """
-
-    print("\n\nadd_all_reserve_constraints")
-    print("===============================")
-    # get all the network reserve areas defined in links.csv
-    reserve_areas = network.links["reserve_area"].dropna()
-    reserve_areas = reserve_areas[reserve_areas != 0]
-    reserve_areas = reserve_areas[reserve_areas != "0"]
-    reserve_areas = reserve_areas.unique()
-
-    if reserve_areas == [0]:
-        reserve_areas = []
-
-    reserve_areas = [
-        area for area in reserve_areas if "reserve_area" in area
-    ]  # remove areas that does not contain "reserve area"
-    print(f"Found the following areas: {reserve_areas}\n")
-
-    # get the reserve level
-    reserve_levels = [col for col in network.links.columns if "reserve_level_" in col]
-    print(f"Found reserve levels: {reserve_levels}\n")
-
-    # find the links per area
-    # split the dict up into "extendable" and "non-extendable"
-    # {'reserve_area_1':
-    #   {
-    #   'non_extendable':
-    #       {
-    #        'links':
-    #                ['Lk10_(B2-B1)', ...],
-    #        'p_nom':
-    #                [1500.0, ...]
-    #       },
-    #   'all_links':
-    #               ['Lk10_(B2-B1)', 'Lk11_(B2-B1)', 'Lk12_(B2-B1)'],
-    #   'extendable':
-    #               ['Lk11_(B2-B1)', 'Lk12_(B2-B1)']}
-    # }
-    area_to_links_lookup = {}
-    for reserve_area in reserve_areas:
-        area_to_links_lookup[reserve_area] = {}
-        area_to_links_lookup[reserve_area]["non_extendable"] = {}
-        area_to_links_lookup[reserve_area]["all_links"] = []
-        links = network.links
-        area_to_links_lookup[reserve_area]["all_links"] = links[
-            (links["reserve_area"] == reserve_area)
-        ].index.to_list()
-        area_to_links_lookup[reserve_area]["extendable"] = links[
-            (links["reserve_area"] == reserve_area)
-            & (links["p_nom_extendable"] == True)
-        ].index.to_list()
-        area_to_links_lookup[reserve_area]["non_extendable"]["links"] = links[
-            (links["reserve_area"] == reserve_area)
-            & (links["p_nom_extendable"] == False)
-        ].index.to_list()
-        area_to_links_lookup[reserve_area]["non_extendable"]["p_nom"] = links[
-            (links["reserve_area"] == reserve_area)
-            & (links["p_nom_extendable"] == False)
-        ].p_nom.to_list()
-    print("The areas are associated  with the links:")
-    pprint(area_to_links_lookup)
-    print("\n")
-
-    # read reserves RHS
-    reserves = pd.read_csv(reserves_file_path, index_col=0)
-    print(f"Reading RHS file from: {reserves_file_path}\n")
-    print("Found RHS reserve constraints:")
-    print(f"{reserves}\n")
-
-    ignored_constraints = []
-
-    # add constraints found
-    for reserve_level in reserve_levels:
-        for reserve_area in reserve_areas:
-            reserve_level_reserve_area = f"{reserve_level}_{reserve_area}"
-            area_link_list = area_to_links_lookup[reserve_area]
-
-            try:
-                reserve_capacity_needed = reserves.at[
-                    reserve_level_reserve_area, "value_mw"
-                ]
-                constraint_name = f"Reserve_Constraint_{reserve_level_reserve_area}_{reserve_capacity_needed}"
-                print(
-                    f"Adding: {constraint_name}-{area_link_list} >= {reserve_capacity_needed}\n\n"
-                )
-                print("=" * 80)
-                add_reserve_constraint_per_level_and_area(
-                    m,
-                    network,
-                    reserve_level,
-                    area_link_list,
-                    reserve_capacity_needed,
-                    constraint_name,
-                )
-            except KeyError:
-                print(
-                    f"{reserve_level} and {reserve_area} was not found in reserves.csv file. Skipping. Please check.\n"
-                )
-                ignored_constraints.append(reserve_level_reserve_area)
-
-    print("Final Linopy Model for optimization:")
-    print("Call: network.optimize.solve_model()")
-    print("============================================================")
-    print("The following constraints have been ADDED to the model:")
-    added_constraints = [c for c in m.constraints if "Reserve_Constraint_" in c]
-    for i, constraint in enumerate(added_constraints):
-        print(f"{i} ✅ -- {constraint}")
-    print("============================================================")
-    print("The following constraints have been SKIPPED:")
-    for i, constraint in enumerate(ignored_constraints):
-        print(f"{i} ❌ -- {constraint}")
-    print("============================================================\n\n")
+# def add_reserve_constraint_per_level_and_area(
+#     m: linopy.model,
+#     network: pypsa.Network,
+#     reserve_level: str,
+#     area_link_list: dict,
+#     reserve_capacity_needed: float,
+#     constraint_name: str,
+# ):
+#     """add a single reserve constraint, this is called by the constaints setup function and not used by user"""
+#
+#     # Dispatch variable
+#     # All links are dispatched
+#     Total_Dispatch = m.variables["Link-p"] * network.links[reserve_level]
+#
+#     Dispatch = Total_Dispatch.sel(Link=area_link_list["all_links"]).sum("Link")
+#
+#     # Scheduled extendable variable
+#     # only extendable links is added
+#     # Meet a reserve level in a area (extendable and non extandable ) total needs to meet the given reserve requirement
+#     Total_Schedule_Extendable = (
+#         m.variables["Link-p_nom"].sel(
+#             {"Link-ext": area_link_list["extendable"]}
+#         )  # Select all the extendable links
+#         * network.links.loc[area_link_list["extendable"]][
+#             reserve_level
+#         ].to_list()  # Multiply each extendable link with its reserve level
+#     )
+#     Schedule_Extendable = (
+#         Total_Schedule_Extendable.sum()
+#     )  # Get the total extendable TODO - what type of var is this
+#
+#     # Scheduled non-extendable variable (scalar)
+#     # scheduled non-extendable totel is the sum of all the p_nom * reserve_level
+#     # contribution of each link to reserves
+#     non_extantable_total_mw = sum(
+#         area_link_list["non_extendable"]["p_nom"]
+#         * network.links.loc[area_link_list["non_extendable"]["links"]][reserve_level]
+#     )
+#
+#     # build constraint sentence
+#     # check if there are links, but if the non extendable total is 0 don't bother
+#     if (
+#         len(area_link_list["non_extendable"]["links"]) > 0
+#         and non_extantable_total_mw > 0
+#     ):
+#         # If extendable then build new capacity until you meet reserve requirement
+#         print(
+#             f"Total adjusted non extendable contrib to reserves {area_link_list['non_extendable']['links']}- {non_extantable_total_mw}"
+#         )
+#         Reserve_Constraint = (
+#             Schedule_Extendable >= reserve_capacity_needed - non_extantable_total_mw
+#         )
+#     else:
+#         Reserve_Constraint = Schedule_Extendable >= reserve_capacity_needed
+#
+#     # add the constraint to the linopy model
+#     constraint = m.add_constraints(Reserve_Constraint, name=constraint_name)
+#
+#     print(f"{constraint}\n\n")
+#
+#
+# def add_all_reserve_constraints(
+#     network: pypsa.Network, m: linopy.model, reserves_file_path: str | Path
+# ):
+#     """find all constraints from links.csv, and loads reserves.csv witht the RHS from Inputsfolder
+#
+#     To set up, in the links.csv file:
+#     1. add a column called "reserve_area" and add the reserve areas to this column e.g - reserve_area_1, WesternCape, Gauteng. These names will be used in 'reserves.csv'.
+#     2. add columns called "reserve_level_1", "reserve_level_2", "reserve_level_n" and add the weightings e.g. 0.2, 0.3. These names will be used in 'reserves.csv'.
+#
+#     links.csv
+#     reserve_area        reserve_level_1         reserve_level_2     .....
+#     ============        ===============         ===============     =====
+#     reserve_area_1      0.9                     0.1
+#     WesternCape         0                       0.7
+#     Gauteng             0.4                     0.2
+#
+#
+#     3. Create a 'reserves.csv' file and use the 'reserve_area' and 'reserve_level' names to build the file and column 'level_area'
+#     the level_area column should be 'reserve_level_name_reserve_area'. The 'value_mw' is the RHS of the constraint
+#
+#     reserves.csv
+#     level_area                          value_mw
+#     ------------------------------      --------
+#     reserve_level_1_reserve_area_1      100
+#     reserve_level_1_WesternCape         30
+#     reserve_level_1_Gauteng             20
+#
+#     """
+#
+#     print("\n\nadd_all_reserve_constraints")
+#     print("===============================")
+#     # get all the network reserve areas defined in links.csv
+#     reserve_areas = network.links["reserve_area"].dropna()
+#     reserve_areas = reserve_areas[reserve_areas != 0]
+#     reserve_areas = reserve_areas[reserve_areas != "0"]
+#     reserve_areas = reserve_areas.unique()
+#
+#     if reserve_areas == [0]:
+#         reserve_areas = []
+#
+#     reserve_areas = [
+#         area for area in reserve_areas if "reserve_area" in area
+#     ]  # remove areas that does not contain "reserve area"
+#     print(f"Found the following areas: {reserve_areas}\n")
+#
+#     # get the reserve level
+#     reserve_levels = [col for col in network.links.columns if "reserve_level_" in col]
+#     print(f"Found reserve levels: {reserve_levels}\n")
+#
+#     # find the links per area
+#     # split the dict up into "extendable" and "non-extendable"
+#     # {'reserve_area_1':
+#     #   {
+#     #   'non_extendable':
+#     #       {
+#     #        'links':
+#     #                ['Lk10_(B2-B1)', ...],
+#     #        'p_nom':
+#     #                [1500.0, ...]
+#     #       },
+#     #   'all_links':
+#     #               ['Lk10_(B2-B1)', 'Lk11_(B2-B1)', 'Lk12_(B2-B1)'],
+#     #   'extendable':
+#     #               ['Lk11_(B2-B1)', 'Lk12_(B2-B1)']}
+#     # }
+#     area_to_links_lookup = {}
+#     for reserve_area in reserve_areas:
+#         area_to_links_lookup[reserve_area] = {}
+#         area_to_links_lookup[reserve_area]["non_extendable"] = {}
+#         area_to_links_lookup[reserve_area]["all_links"] = []
+#         links = network.links
+#         area_to_links_lookup[reserve_area]["all_links"] = links[
+#             (links["reserve_area"] == reserve_area)
+#         ].index.to_list()
+#         area_to_links_lookup[reserve_area]["extendable"] = links[
+#             (links["reserve_area"] == reserve_area)
+#             & (links["p_nom_extendable"] == True)
+#         ].index.to_list()
+#         area_to_links_lookup[reserve_area]["non_extendable"]["links"] = links[
+#             (links["reserve_area"] == reserve_area)
+#             & (links["p_nom_extendable"] == False)
+#         ].index.to_list()
+#         area_to_links_lookup[reserve_area]["non_extendable"]["p_nom"] = links[
+#             (links["reserve_area"] == reserve_area)
+#             & (links["p_nom_extendable"] == False)
+#         ].p_nom.to_list()
+#     print("The areas are associated  with the links:")
+#     pprint(area_to_links_lookup)
+#     print("\n")
+#
+#     # read reserves RHS
+#     reserves = pd.read_csv(reserves_file_path, index_col=0)
+#     print(f"Reading RHS file from: {reserves_file_path}\n")
+#     print("Found RHS reserve constraints:")
+#     print(f"{reserves}\n")
+#
+#     ignored_constraints = []
+#
+#     # add constraints found
+#     for reserve_level in reserve_levels:
+#         for reserve_area in reserve_areas:
+#             reserve_level_reserve_area = f"{reserve_level}_{reserve_area}"
+#             area_link_list = area_to_links_lookup[reserve_area]
+#
+#             try:
+#                 reserve_capacity_needed = reserves.at[
+#                     reserve_level_reserve_area, "value_mw"
+#                 ]
+#                 constraint_name = f"Reserve_Constraint_{reserve_level_reserve_area}_{reserve_capacity_needed}"
+#                 print(
+#                     f"Adding: {constraint_name}-{area_link_list} >= {reserve_capacity_needed}\n\n"
+#                 )
+#                 print("=" * 80)
+#                 add_reserve_constraint_per_level_and_area(
+#                     m,
+#                     network,
+#                     reserve_level,
+#                     area_link_list,
+#                     reserve_capacity_needed,
+#                     constraint_name,
+#                 )
+#             except KeyError:
+#                 print(
+#                     f"{reserve_level} and {reserve_area} was not found in reserves.csv file. Skipping. Please check.\n"
+#                 )
+#                 ignored_constraints.append(reserve_level_reserve_area)
+#
+#     print("Final Linopy Model for optimization:")
+#     print("Call: network.optimize.solve_model()")
+#     print("============================================================")
+#     print("The following constraints have been ADDED to the model:")
+#     added_constraints = [c for c in m.constraints if "Reserve_Constraint_" in c]
+#     for i, constraint in enumerate(added_constraints):
+#         print(f"{i} ✅ -- {constraint}")
+#     print("============================================================")
+#     print("The following constraints have been SKIPPED:")
+#     for i, constraint in enumerate(ignored_constraints):
+#         print(f"{i} ❌ -- {constraint}")
+#     print("============================================================\n\n")
 
 
 def add_min_stable_gen(network: pypsa.Network, m):

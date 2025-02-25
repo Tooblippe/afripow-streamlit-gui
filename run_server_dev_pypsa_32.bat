d6b20c944f1ae2fcb62f0600b8b9dff36d31fda1
tooblippe <tobie.nortje@gmail.com>
2025-02-25 19:35:30 +0200

    Reserves new

--- run_server_dev_pypsa_32.bat
+++ run_server_dev_pypsa_32.bat
@@ -1,5 +1,4 @@
 call C:\Users\apvse\anaconda3\condabin\activate.bat pypsa_v32_py312
-REM python -m pip install -i https://test.pypi.org/simple/ afripow-pypsa --upgrade
 git pull
 git checkout dev
 streamlit run C:\Users\apvse\OneDrive\afripow-streamlit-gui-dev\PypsaGui.py --server.runOnSave=True --server.port=8999
